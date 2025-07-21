fc0c97b39ce8b047cfb2bd178e30d837330ac219
hiphop5782 <zdlsz@hanmail.net>
2025-07-21 14:48:23 +0900

    클릭미러링 가능성 발견

--- topwar4j/src/test/java/com/hacademy/topwar/Test31클릭미러링.java
+++ topwar4j/src/test/java/com/hacademy/topwar/Test31클릭미러링.java
@@ -7,69 +7,52 @@ import com.github.kwhat.jnativehook.GlobalScreen;
 import com.github.kwhat.jnativehook.NativeHookException;
 import com.github.kwhat.jnativehook.mouse.NativeMouseEvent;
 import com.github.kwhat.jnativehook.mouse.NativeMouseInputListener;
-import com.hacademy.topwar.macro.MacroStatus;
-import com.hacademy.topwar.util.PropertyManager;
-import com.hacademy.topwar.util.RectData;
-import com.hacademy.topwar.util.User32Ex;
 import com.hacademy.topwar.util.WinUserEx;
+import com.sun.jna.Native;
 import com.sun.jna.platform.win32.User32;
-import com.sun.jna.platform.win32.WinDef.HWND;
 import com.sun.jna.platform.win32.WinDef.LPARAM;
-import com.sun.jna.platform.win32.WinDef.POINT;
+import com.sun.jna.platform.win32.WinDef.RECT;
 import com.sun.jna.platform.win32.WinDef.WPARAM;
 
 
 public class Test31클릭미러링 {
 	public static void main(String[] args) throws NativeHookException {
-		MacroStatus status = PropertyManager.getMacroStatus();
-		List<RectData> screenList = status.getScreenList();
+		List<Rectangle> rectList = List.of(
+			new Rectangle(0, 0, 200, 200),
+			new Rectangle(200, 0, 200, 200),
+			new Rectangle(400, 0, 200, 200)
+		);
 		
 		NativeMouseInputListener listener = new NativeMouseInputListener() {
 			@Override
 			public void nativeMouseClicked(NativeMouseEvent e) {
-				RectData target = null;
-				for(RectData data : screenList) {
-					Rectangle rect = data.toRectangle();
-					if(rect.contains(e.getPoint())) {
-						target = data;
-						break;
-					}
-				}
-				if(target == null) return;
 				
-				System.out.println(target);
-				
-				//상대좌표 환산
-				int offsetX = e.getPoint().x - target.x;
-				int offsetY = e.getPoint().y - target.y;
-				System.out.println("클릭한 절대좌표 : (" + e.getPoint()+")");
-				System.out.println("클릭한 상대좌표 : ("+offsetX+", "+offsetY+")");
-				
-				//클릭 처리
-				for(RectData data : screenList) {
-					if(data == target) continue;
-					
-					Rectangle rect = data.toRectangle();
-					int targetX = rect.x + offsetX;
-					int targetY = rect.y + offsetY;
-					System.out.println("target ("+targetX+", "+targetY+")");
-					
-					POINT pt = new POINT();
-					pt.x = targetX;
-					pt.y = targetY;
-					
-					HWND hwnd = User32Ex.INSTANCE.WindowFromPoint(pt);
-					System.out.println("hwnd = " + hwnd);
-					if(hwnd == null) continue;
-					
-					// 클릭 메시지 전송
-		            int lParam = (targetY << 16) | (targetX & 0xFFFF);
-		            User32.INSTANCE.SendMessage(hwnd, WinUserEx.WM_LBUTTONDOWN, new WPARAM(0), new LPARAM(lParam));
-		            try { Thread.sleep(50); } catch(Exception ex) {}
-		            User32.INSTANCE.SendMessage(hwnd, WinUserEx.WM_LBUTTONUP, new WPARAM(0), new LPARAM(lParam));
+				//창 열거 및 필터링
+				User32.INSTANCE.EnumWindows((hwnd, data)->{
+					char[] buffer = new char[512];
+					User32.INSTANCE.GetWindowText(hwnd, buffer, buffer.length);
+					String title = Native.toString(buffer);
+					if(title.contains("Topwar")) {
+						System.out.println("title = " + title);
+						
+						RECT rect = new RECT();
+						User32.INSTANCE.GetWindowRect(hwnd, rect);
+						
+						int x = e.getX() % 200;
+						int y = e.getY() % 200;
+						int lParam = (y << 16) | (x & 0xFFFF);
 
-		            System.out.println("Clicked at: " + targetX + ", " + targetY);
-				}
+		                // 3. 클릭 메시지 보내기
+		                System.out.println("Clicking at: " + x + ", " + y);
+		                User32.INSTANCE.SendMessage(hwnd, WinUserEx.WM_LBUTTONDOWN, new WPARAM(0), new LPARAM(lParam));
+		                try {
+							Thread.sleep(50);
+						} catch (InterruptedException e1) {}
+		                User32.INSTANCE.SendMessage(hwnd, WinUserEx.WM_LBUTTONUP, new WPARAM(0), new LPARAM(lParam));
+					}
+					return true;
+				}, null);
+				
 			}
 			@Override
 			public void nativeMouseDragged(NativeMouseEvent e) {
